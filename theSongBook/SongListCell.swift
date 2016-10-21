604035aba4c7210b25d2e15dac713090c9d75afc
hanwool <hanwool@scworks.io>
2016-10-22 07:57:00 +0900

    UI prototype

---
+++ theSongBook/SongListCell.swift
@@ -0,0 +1,16 @@
+//
+//  SongListCell.swift
+//  theSongBook
+//
+//  Created by seyriz on 2016. 10. 22..
+//  Copyright © 2016년 seyriz. All rights reserved.
+//
+
+import UIKit
+
+class SongListCell: UITableViewCell {
+    
+    @IBOutlet weak var songNumber: UILabel!
+    @IBOutlet weak var songTitle: UILabel!
+    @IBOutlet weak var singer: UILabel!
+}
--- theSongBook/ViewController.swift
+++ theSongBook/ViewController.swift
@@ -8,10 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController ,UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate{
+    
+    
+    @IBOutlet weak var searchOption: UISegmentedControl!
+    @IBOutlet weak var karaokeProvider: UISegmentedControl!
+    @IBOutlet weak var searchValue: UISearchBar!
+    @IBOutlet weak var searchCategory: UISegmentedControl!
+    @IBOutlet weak var songList: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        songList.delegate = self
+        songList.dataSource = self
+        searchValue.delegate = self
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +30,40 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        
+    }
+    
+    func searchValue(searchValue: UISearchBar, textDidChange searchText: String) {
+        
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0;
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> SongListCell {
+        let cell = songList.dequeueReusableCellWithIdentifier("SongListCell") as SongListCell;
+        
+        return cell;
+    }
 
 
 }
-
